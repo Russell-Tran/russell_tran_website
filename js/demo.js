b28ded4cc664efd52c4097376ac3fa692bd78ffe
Seah Ying Hang <seahyinghang8@gmail.com>
2022-11-27 00:39:41 -0800

    Template completed

--- js/demo.js
+++ js/demo.js
@@ -4,4 +4,52 @@ document.addEventListener('DOMContentLoaded', function () {
     dotColor: '#5cbdaa',
     lineColor: '#5cbdaa'
   });
+
+  document.querySelectorAll(".portfolio-filter>button").forEach(
+    (elem) => {
+      elem.addEventListener("click", () => {
+        // Remove the active class from other filters
+        const activeClassName = "active-filter";
+        document.querySelector(`.${activeClassName}`).classList.remove(activeClassName);
+        elem.classList.add(activeClassName);
+        const tagText = elem.textContent.trim().toLowerCase();
+        if (tagText == "see all") {
+          showAllProjects();
+        } else {
+          filterProjectsByTag(tagText);
+        }
+      })
+    }
+  )
 }, false);
+
+
+function showAllProjects() {
+  document.querySelectorAll(".projects>div").forEach(
+    (elem) => {
+      elem.style = "display: block";
+    }
+  );
+}
+
+
+function filterProjectsByTag(filteredTag) {
+  document.querySelectorAll(".projects>div").forEach(
+    (elem) => {
+      let containsFilter = false;
+      elem.querySelectorAll(".p-tags > div").forEach(
+        (tag) => {
+          const tagText = tag.textContent.trim().toLowerCase();
+          if (tagText == filteredTag) {
+            containsFilter = true;
+          }
+        }
+      )
+      if (containsFilter) {
+        elem.style = "display: block";
+      } else {
+        elem.style = "display: none";
+      }
+    }
+  )
+}
